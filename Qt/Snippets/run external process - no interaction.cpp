60fb316501e33f2add16256ec53097c8b34e8913
Brandon Marzolf <bmarzolf@globusmedical.com>
2018-03-15 13:25:54 -0400

    qt updates

---
+++ Qt/Snippets/run external process - no interaction.cpp
@@ -0,0 +1,2 @@
+// yes it's that easy. it will run, it will terminate
+QPRocess::execute(file, args);
--- Qt/Snippets/run external process.cpp
+++ Qt/Snippets/run external process.cpp
@@ -1,5 +1,5 @@
 // make sure "this" is provided to the QProcess() so that it has a parent
 // which will make the memory clean up automatic
 QProcess *process = new QProcess(this);
-process->execute("path/to/your/script.run");
+process->start("path/to/your/script.run");
 process->waitForFinished();

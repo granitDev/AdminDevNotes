0fb0a7f4b0fd308987a444096612d9c5297e098f
Brandon Marzolf <bmarzolf@globusmedical.com>
2018-04-24 13:48:29 -0400

    cpp updates

---
+++ CPP/ternary operator or inline if statement.cpp
@@ -0,0 +1,5 @@
+// You can use this to pick which value to assign to a variable:
+int foo = (bar > bash) ? bar : bash;
+
+// Or even which variable to assign a value to:
+((bar > bash) ? bar : bash) = foo;
